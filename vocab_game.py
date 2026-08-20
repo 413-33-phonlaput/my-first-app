5e161c380fa25f1a99c150f75cad3144dc166302
413-33-phonlaput <56875@yupparaj.ac.th>
2026-08-20 09:50:55 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -1,4 +1,4 @@
-import time
+simport time
 import streamlit as st
  
 st.title("⏱️ เกมเติมศัพท์จับเวลา")
@@ -39,36 +39,36 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
  
  # ตรวจข้อ 1
  if u_ans1 == "apple":
- st.success("✅ ข้อ 1: ถูกต้อง")
- score += 1
+   st.success("✅ ข้อ 1: ถูกต้อง")
+   score += 1
  else:
  st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
  
  # ตรวจข้อ 2
  if u_ans2 == "fish":
- st.success("✅ ข้อ 2: ถูกต้อง")
- score += 1
+   st.success("✅ ข้อ 2: ถูกต้อง")
+   score += 1
  else:
  st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
  
  # ตรวจข้อ 3
  if u_ans3 == "banana":
- st.success("✅ ข้อ 3: ถูกต้อง")
- score += 1
+   st.success("✅ ข้อ 3: ถูกต้อง")
+   score += 1
  else:
  st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
  
  # ตรวจข้อ 4
  if u_ans4 == "carrot":
- st.success("✅ ข้อ 4: ถูกต้อง")
- score += 1
+   st.success("✅ ข้อ 4: ถูกต้อง")
+   score += 1
  else:
  st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
  
  st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
  
  if score == 4:
- st.success("🎉 You win!")
+   st.success("🎉 You win!")
  else:
  st.error("💀 You lose!")
  
